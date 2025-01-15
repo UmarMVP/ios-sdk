b96a47d3664cc46584e6c8b89d91b42b98cc7e09
UmarMVP <134934402+UmarMVP@users.noreply.github.com>
2025-01-15 13:14:35 +0400

    bug fix (#1)
    Co-authored-by: mohamed <umar20599@gmail.com>

--- Sources/Heatshrink/include/heatshrink-Bridging-Header.h
+++ Sources/Heatshrink/include/heatshrink-Bridging-Header.h
@@ -1,7 +1,7 @@
 #ifndef heatshrink-Bridging-Header.h
 #define heatshrink-Bridging-Header.h
 
-#import <RNHeatshrinkDecoder.h>
-#import <RNHeatshrinkEncoder.h>
+#import "RNHeatshrinkDecoder.h"
+#import "RNHeatshrinkEncoder.h"
 
 #endif
